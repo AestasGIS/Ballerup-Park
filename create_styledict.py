d92f383699e4016b625d0ade98d939b00aad5412
Bo Victor Thomsen <bo.victor.thomsen@gmail.com>
2025-01-19 13:41:31 +0100

    Update create_styledict.py

--- create_styledict.py
+++ create_styledict.py
@@ -8,6 +8,11 @@ db_port="5432"
 db_styletable="sweco_jsondata.styleinfo"    
 resultfile = 'd:/projekter/ballerup/ballerup_style.json'
 
+CLASS_POINT = '[{{"sortid":10,"name":"{name}","expression":"","label":false,"label_size":"","label_color":"","color":"{color}","outlinecolor":"{ocolor}","symbol":"{symbol}","angle":"","size":"{size}","width":"{width}","overlaycolor":"","overlayoutlinecolor":"","overlaysymbol":"","overlaysize":"","overlaywidth":"","label_text":"","label_position":"","label_font":"","label_fontweight":"","label_angle":"","label_backgroundcolor":"","style_opacity":"","overlaystyle_opacity":"","label_force":""}}]'
+CLASS_LINE=   '[{{"sortid":10,"name":"{name}","expression":"","label":false,"label_size":"","label_color":"","color":"{color}","outlinecolor":"{ocolor}","symbol":"{symbol}","angle":"","size":"{size}","width":"{width}","overlaycolor":"","overlayoutlinecolor":"","overlaysymbol":"","overlaysize":"","overlaywidth":"","label_text":"","label_position":"","label_font":"","label_fontweight":"","label_angle":"","label_backgroundcolor":"","style_opacity":"","overlaystyle_opacity":"","label_force":""}}]'
+CLASS_POLYGON='[{{"sortid":10,"name":"{name}","expression":"","label":false,"label_size":"","label_color":"","color":"{color}","outlinecolor":"{ocolor}","symbol":"{symbol}","angle":"","size":"{size}","width":"{width}","overlaycolor":"","overlayoutlinecolor":"","overlaysymbol":"","overlaysize":"","overlaywidth":"","label_text":"","label_position":"","label_font":"","label_fontweight":"","label_angle":"","label_backgroundcolor":"","style_opacity":"","overlaystyle_opacity":"","label_force":false,"id":0,"class_minscaledenom":"","class_maxscaledenom":"","leader":false,"leader_gridstep":"","leader_maxdistance":"","leader_color":"","pattern":"","linecap":"","geomtransform":"","minsize":"","maxsize":"","style_offsetx":"","style_offsety":"","style_polaroffsetr":"","style_polaroffsetd":"","overlaypattern":"","overlaylinecap":"","overlayangle":"","overlaygeomtransform":"","overlayminsize":"","overlaymaxsize":"","overlaystyle_offsetx":"","overlaystyle_offsety":"","overlaystyle_polaroffsetr":"","overlaystyle_polaroffsetd":"","label_minscaledenom":"","label_maxscaledenom":"","label_outlinecolor":"","label_buffer":"","label_repeatdistance":"","label_backgroundpadding":"","label_offsetx":"","label_offsety":"","label_expression":"","label_maxsize":"","label_minfeaturesize":"","label2":false,"label2_text":"","label2_force":false,"label2_minscaledenom":"","label2_maxscaledenom":"","label2_position":"","label2_size":"","label2_font":"","label2_fontweight":"","label2_color":"","label2_outlinecolor":"","label2_buffer":"","label2_repeatdistance":"","label2_angle":"","label2_backgroundcolor":"","label2_backgroundpadding":"","label2_offsetx":"","label2_offsety":"","label2_expression":"","label2_maxsize":"","label2_minfeaturesize":""}}]'  
+
+
 # Imports
 
 import xmltodict
@@ -23,9 +28,7 @@ def cnv2hex(ind):
     return hex.replace ('0x','')
 
 
-# Forbindelse til stiltabel for SWECO
 # For hver stil 
-# Opret tomt dict element
 # Find key, goup, type
 # læg diise plus org dict ind i samle dict med nøgle key
 # Konverter dict til json for mapcentia
@@ -36,28 +39,75 @@ def cnv2hex(ind):
 # main
 
 # Connection to DB
-
 conn = psycopg2.connect(database=database, host=db_host, user=db_user, password=db_pass, port=db_port)
 cur = conn.cursor()
+
+# Create empty styles dict to be filled
+styles = {}
 
 cur.execute('SELECT * FROM {}'.format(db_styletable))
 rows = cur.fetchall()
 
-styles = {}
-
+# For every style in table..
 for r in rows:
 
-    d={}
-    d['key']=str(r[0])
-    d['name']=str(r[1])
-    d['groupname']=str(r[2])
-    gc = str(r[3]).replace('class>','geoclass>')
-    if gc != 'None' and gc != '':
-        org = xmltodict.parse(gc)
-        d['sweco'] = org['geoclass'] 
-        styles[d['key'].upper()] = d 
-        
+    # Check if geoclass is empty
+        if str(r[3]) != 'None' and str(r[3]) != '':
+            # Create one-style empty dict and main elements
+            d={}
+            d['key']=str(r[0])
+            d['name']=str(r[1])
+            d['groupname']=str(r[2])
+    
+            # Convert geoclass to MapCentia class.
+            gc = str(r[3]).replace('class>','geoclass>')
+            org = xmltodict.parse(gc)
+            
+            # Save org. SWECO class definition
+            d['sweco'] = org['geoclass']
+                        
+
+            # Point type
+            if d['groupname'] == 'Point':
+
+
+            # Line type
+            if d['groupname'] == 'Line':
+
+
+            # Polygon type or Workarea type
+            if d['groupname'] == 'Workarea' or d['groupname'] == 'Polygon':
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+    
+    
+    
+    
+    
+    
+    
+            # Save style under key value in styles dict.  
+            styles[d['key'].upper()] = d
+
+#Open save file.
 out_file = open("styles.json", "w")
+
+# Save styles dict as json in file
 json.dump(styles, out_file, indent = 4)
 out_file.close()
         
@@ -149,6 +199,3 @@ out_file.close()
 
 
 
-CLASS_POINT = '[{{"sortid":10,"name":"{name}","expression":"","label":false,"label_size":"","label_color":"","color":"{color}","outlinecolor":"{ocolor}","symbol":"{symbol}","angle":"","size":"{size}","width":"{width}","overlaycolor":"","overlayoutlinecolor":"","overlaysymbol":"","overlaysize":"","overlaywidth":"","label_text":"","label_position":"","label_font":"","label_fontweight":"","label_angle":"","label_backgroundcolor":"","style_opacity":"","overlaystyle_opacity":"","label_force":""}}]'
-CLASS_LINE=   '[{{"sortid":10,"name":"{name}","expression":"","label":false,"label_size":"","label_color":"","color":"{color}","outlinecolor":"{ocolor}","symbol":"{symbol}","angle":"","size":"{size}","width":"{width}","overlaycolor":"","overlayoutlinecolor":"","overlaysymbol":"","overlaysize":"","overlaywidth":"","label_text":"","label_position":"","label_font":"","label_fontweight":"","label_angle":"","label_backgroundcolor":"","style_opacity":"","overlaystyle_opacity":"","label_force":""}}]'
-CLASS_POLYGON='[{{"sortid":10,"name":"{name}","expression":"","label":false,"label_size":"","label_color":"","color":"{color}","outlinecolor":"{ocolor}","symbol":"{symbol}","angle":"","size":"{size}","width":"{width}","overlaycolor":"","overlayoutlinecolor":"","overlaysymbol":"","overlaysize":"","overlaywidth":"","label_text":"","label_position":"","label_font":"","label_fontweight":"","label_angle":"","label_backgroundcolor":"","style_opacity":"","overlaystyle_opacity":"","label_force":false,"id":0,"class_minscaledenom":"","class_maxscaledenom":"","leader":false,"leader_gridstep":"","leader_maxdistance":"","leader_color":"","pattern":"","linecap":"","geomtransform":"","minsize":"","maxsize":"","style_offsetx":"","style_offsety":"","style_polaroffsetr":"","style_polaroffsetd":"","overlaypattern":"","overlaylinecap":"","overlayangle":"","overlaygeomtransform":"","overlayminsize":"","overlaymaxsize":"","overlaystyle_offsetx":"","overlaystyle_offsety":"","overlaystyle_polaroffsetr":"","overlaystyle_polaroffsetd":"","label_minscaledenom":"","label_maxscaledenom":"","label_outlinecolor":"","label_buffer":"","label_repeatdistance":"","label_backgroundpadding":"","label_offsetx":"","label_offsety":"","label_expression":"","label_maxsize":"","label_minfeaturesize":"","label2":false,"label2_text":"","label2_force":false,"label2_minscaledenom":"","label2_maxscaledenom":"","label2_position":"","label2_size":"","label2_font":"","label2_fontweight":"","label2_color":"","label2_outlinecolor":"","label2_buffer":"","label2_repeatdistance":"","label2_angle":"","label2_backgroundcolor":"","label2_backgroundpadding":"","label2_offsetx":"","label2_offsety":"","label2_expression":"","label2_maxsize":"","label2_minfeaturesize":""}}]'  
